7841c72272d71c7fb6b03cc2e8c126a56dae5f49
Ivan Urban <ivanurban_bg@yahoo.com>
2021-09-01 17:19:50 +0200

    Update urls.py

--- useraccount/urls.py
+++ useraccount/urls.py
@@ -12,7 +12,7 @@ app_name = 'useraccount'
 urlpatterns = [
 
 
-    # path('', include('django.contrib.auth.urls')),
+  
 
     path('register/', views.register, name='register'),
     #login logout urpls
@@ -27,10 +27,10 @@ urlpatterns = [
 
 
     # reset password urls
-    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
-    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html', success_url=reverse_lazy("useraccount:password_reset_done")),name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html",success_url = reverse_lazy('useraccount:password_reset_complete')), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 
 
     path('edit/', views.edit, name='edit'),
