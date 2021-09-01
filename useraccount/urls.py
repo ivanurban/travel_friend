7505324d28326b711dd893ec29ede2dbe3b38f18
Ivan Urban <ivanurban_bg@yahoo.com>
2021-09-01 14:24:33 +0200

    Update urls.py

--- useraccount/urls.py
+++ useraccount/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
+from django.urls import reverse_lazy
+
 from django.contrib.auth import views as auth_views #to use with Class-based Views
 
 from . import views
@@ -19,7 +21,7 @@ urlpatterns = [
 
 
     # change password urls
-    path('password_change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
+    path('password_change/',auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('useraccount:password_change_done')),name='password_change'),
     path('password_change/done/',auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
 
 
